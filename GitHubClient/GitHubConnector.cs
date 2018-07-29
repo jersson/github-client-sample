a21abf1846eefc6100b01205a4f0d44340d952b0
Jersson <Jersson@gmail.com>
2018-07-29 01:25:54 -0500

    Add GitHub connection

--- GitHubClient/GitHubConnector.cs
+++ GitHubClient/GitHubConnector.cs
@@ -5,9 +5,18 @@ namespace GitHubClient
     public class GitHubConnector
     {
         public string GetUserInformation(string username){
-            var result = "";
 
-            return result;
+            var client = new RestSharp.RestClient("https://api.github.com/");
+            var request = new RestSharp.RestRequest("users/{username}", RestSharp.Method.GET);
+
+            request.AddUrlSegment("username", username);
+
+
+            RestSharp.IRestResponse response = client.Execute(request);
+            var content = response.Content; 
+
+
+            return content;
         }
     }
 }
