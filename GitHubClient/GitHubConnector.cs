f7ed6373f685150d1b3d214acd614d7d88b6430d
Jersson <Jersson@gmail.com>
2018-07-29 11:07:53 -0500

    Add console demo client

--- GitHubClient/GitHubConnector.cs
+++ GitHubClient/GitHubConnector.cs
@@ -1,20 +1,21 @@
 using System;
 
+using RestSharp;
+
 namespace GitHubClient
 {
     public class GitHubConnector
     {
         public string GetUserInformation(string username){
 
-            var client = new RestSharp.RestClient("https://api.github.com/");
-            var request = new RestSharp.RestRequest("users/{username}", RestSharp.Method.GET);
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest("users/{username}", RestSharp.Method.GET);
 
             request.AddUrlSegment("username", username);
 
 
-            RestSharp.IRestResponse response = client.Execute(request);
+            IRestResponse response = client.Execute(request);
             var content = response.Content; 
-
 
             return content;
         }
---
+++ GitHubConsoleDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using GitHubClient;
+
+
+namespace GitHubConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Put your GitHub username: ");
+            var username = Console.ReadLine();
+
+            var connector = new GitHubConnector();
+            var gitHubInformation = connector.GetUserInformation(username);
+
+            Console.WriteLine("Hello {0} this is your GitHub information:", username);
+            Console.WriteLine(gitHubInformation);
+        }
+    }
+}
