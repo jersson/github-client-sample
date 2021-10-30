b5fc42eadbed5fbaf5a693cd5ca4802605526901
Jersson <jersson@gmail.com>
2021-10-29 22:22:53 -0500

    refactor: improve constructor strategy

--- GitHubClient/types/UserInformationProperty.cs
+++ GitHubClient/types/UserInformationProperty.cs
@@ -5,23 +5,17 @@ namespace GitHubClient.Types
 {
     public class UserInformationProperty
     {
-        public UserInformationProperty()
-        {
-            Name = string.Empty;
-            Type = string.Empty;
-        }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public UserInformationProperty(): this(string.Empty, string.Empty) {}
         public UserInformationProperty(string name, string type)
         {
             Name = name;
             Type = type;
         }
 
-        public string Name { get; set; }
-
-        public string Type { get; set; }
-
         public override string ToString()
-        {   if (Name.Trim().Length == 0 && Type.Trim().Length == 0) 
+        {   if (Name.Trim().Length == 0 && Type.Trim().Length == 0)
                 return string.Empty;
             
             return $"* {Name}: {Type}";
